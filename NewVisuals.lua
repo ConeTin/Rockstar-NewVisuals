d2ac62ccf4cbb62af31e5a26d0e449a59ac2a604
ConeTin <102159205+ConeTin@users.noreply.github.com>
2024-01-28 21:45:14 +0500

    Add files via upload

---
+++ NewVisuals.lua
@@ -0,0 +1,9 @@
+script:name("New Visuals")
+script:devs({"ConeTin"})
+script:desc("New Vision of Rockstar Visuals")
+
+watermark_mod = module.new("Watermark", "Информация о клиенте")
+
+events.render_2d:set(function(event)
+    
+end)
